a27965c270a6e3ece5edf6af6a1f198ddd72a813
Krzysztof Dec <43911358+surechand@users.noreply.github.com>
2020-04-02 21:49:31 +0200

    Add files via upload

---
+++ Rozwiązania/zad1 rozwiazanie.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <random>
+#include <exception>
+#include <memory>
+
+/* Zadanie nr1
+* Jednym z g³ównych celów korzystania ze inteligentnych wskaŸników 
+* jest automatyczne zwalnianie przez nie pamiêci.
+* Popraw ten program, tak by nie korzysta³ z surowych wskaŸników 
+* poprzez zamienienie ich na wskaŸniki inteligentne.
+*/
+
+struct Odd_exception : public std::exception {
+	const char* what_is_it() const throw () {
+		return "This is odd exception";
+	}
+};
+
+bool check_if_even(int number) {
+	if (number % 2 != 0)
+		throw Odd_exception();
+	return true;
+}
+
+int main()
+{
+	std::unique_ptr<int[]> array(new int[50]);
+	for (int i = 0; i < 50; i++) {
+		std::random_device dev;
+		std::mt19937 rng(dev());
+		std::uniform_int_distribution<std::mt19937::result_type> distribution(1, 100);
+		array[i] = distribution(rng);
+	}
+	try {
+		check_if_even(array[0]);
+		std::cout << "Pamiec zostala zwolniona!" << std::endl;
+	}
+	catch (Odd_exception & e) {
+		std::cout << "Pamiec i tak zostanie zwolniona" << std::endl;
+	}
+
+	return 0;
+}
