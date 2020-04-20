aea521a94ae91be0c7a2a1c8722116923ef86538
BartMat <matusikowy@gmail.com>
2020-04-20 23:45:29 +0200

    !!!!jeszcze jedno zadanko!!!!!

---
+++ Lab7_Wątki/Zadania/Zad4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map> 
+#include <fstream> 
+#include <string> 
+#include <algorithm>
+#include <cctype>
+#include <vector>
+#include <thread>
+using namespace std;
+
+//Wykorzystaj w¹tki i wybrany bardzo d³ugi tekst angielski wczytany z pliku  do zliczania statystyk s³ów znaków w tym interpunkcji  i ngramow.
+//dla  s³ów zaimplementuj z u¿yciem w¹tków  w jednym programie  generacjê  ngramow dla s³ów i o d³ugoœci 2 do 5 wyniki programu statystyki zapisz w osobnych plikach.
+//kolejny w¹tek tego samego programu  niech wytwarza  anagramy z s³ów   d³ugiego tekstu i zapisuje do pliku.
+//wskazówki
+//ngram i anagram to nie to samo
+//https://pl.wikipedia.org/wiki/Anagram
+//https://stackoverflow.com/questions/3656762/n-gram-generation-from-a-sentence
+
+int main()
+{
+
+    return 0;
+}
